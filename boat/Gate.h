b4a5735583569a9bc698e0712cbbc9c649e0f2f7
Souhardya Sardar <Souhardya@protonmail.com>
2018-03-16 02:36:41 +0000

    Update Gate.h

--- boat/Gate.h
+++ boat/Gate.h
@@ -1,11 +1,11 @@
 #ifndef GATE_H
 #define GATE_H
 //Gate 1
-static const char* GATE_HOST = "microsoftcheapkeys.com";
+static const char* GATE_HOST = "abc.com";
 static const char* GATE_PATH = "/css_r/gate.php";
 //Gate 2
-static const char* FALLBACK_GATE_HOST = "localhost";
-static const char* FALLBACK_GATE_PATH = "/spectral/gate.php";
+static const char* FALLBACK_GATE_HOST = "localhost"; // here is your game 
+static const char* FALLBACK_GATE_PATH = "/UBoat/gate.php";
 
 static const int POLL_RATE = (1000 * 63 * 1);
 
